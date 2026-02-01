1df02ceec89c93d1aa074b54465e720e0cff95ef
Pallavi165 <pallavibpallavib16@gmail.com>
2026-02-01 19:12:25 +0530

    Add files via upload

---
+++ TourismManagementSystem/src/dto/DestinationDTO.java
@@ -0,0 +1,26 @@
+package dto;
+
+public class DestinationDTO {
+    private int destId;
+    private String name;
+    private String location;
+
+    public int getDestId() {
+        return destId;
+    }
+    public void setDestId(int destId) {
+        this.destId = destId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
